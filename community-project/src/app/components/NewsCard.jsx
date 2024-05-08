4cce57fd9b89837a243b8a37ddff31a2d1bca618
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-05-08 17:31:45 +0000

    added styles to NewsCard.jsx

--- community-project/src/app/components/NewsCard.jsx
+++ community-project/src/app/components/NewsCard.jsx
@@ -3,13 +3,16 @@ import Card from 'react-bootstrap/Card';
  export default function NewsCard({article}) {
 
         return (
-            <Card >
+
+          
+            <Card style={{width:"380px", backgroundColor:"lightgray"}} >
               <Card.Img variant="top" src={article.image}/>
               <Card.Body>
                 <Card.Title>{article.title}</Card.Title>
                 <Card.Text>
                   {article.description}
                 </Card.Text>
+                <a href={article.url}>Read more</a>
               </Card.Body>
             </Card>
           );
