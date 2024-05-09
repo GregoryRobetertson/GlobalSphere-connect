6050a08af45187d5184d77de3596105f87cce8df
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-05-09 03:57:54 +0000

    updated

--- src/app/components/Sponsors.jsx
+++ src/app/components/Sponsors.jsx
@@ -1,37 +1,29 @@
-'use client'
-
 import Image from "next/image";
 import { sponsors } from "./sponsorsList";
 
-export default function() {
+
+export default function Sponsors() {
     return (
         <section className="m-5">
-        <h2>Meet Our Sponsors</h2>
-        <div className="flex flex-wrap justify center gap-4 p-5">
-
-           {sponsors.map((sponsor, index) => (
-<div key={index}> 
-<Image
-width={150}
-height={150}
-src={`/${sponsor.src}.png`}
-alt={sponsor.name + "Logo"}
-className="rounded-lg shawdow-lg"
-/>
-<h3 className="p-1.5">{sponsor.name}</h3>
-<p>{sponsor.description}</p>
-<p>{sponsor.contact}</p>
-</div>
-
-           ))}
-        </div>
-   </section>
-  
-
-
-
-
-      
-       
-    )
+            <h2>Meet Our Sponsors</h2>
+            <div className="flex flex-wrap justify-center gap-4 p-5">
+                {sponsors.map((sponsor, index) => (
+                    <div key={index}> 
+                        <Image
+                            width={150}
+                            height={150}
+                            src={`/${sponsor.src}.png`}
+                            alt={sponsor.name + "Logo"}
+                            className="rounded-lg shadow-lg"
+                        />
+                        <h3 className="p-1.5">{sponsor.name}</h3>
+                        <p>{sponsor.description}</p>
+                        <p>{sponsor.contact}</p>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
 }
+
+Sponsors.displayName = 'Sponsors';
