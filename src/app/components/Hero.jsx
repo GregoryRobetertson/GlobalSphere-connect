f20db033bd040f95affd779479d20393091b0d89
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-05-10 19:22:15 +0000

    updated hero

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,14 +1,14 @@
 'use client';
-import react from "react";
-import LogInWithGoogle from "./LoginWithGoogle";
+
+
 export default function Hero({getuser}) {
     return (
         <section className="py-10 px-5 bg-blue-100 text-center">
-            <h2 className="text-3x1 font-bold text-gray-800 mb-3">Welcome to our Commuinity Marketplace</h2>
-            <p className="text-gray-700 mb-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint illum aliquid dolorem laboriosam repellat, expedita vel distinctio a blanditiis aspernatur animi consequuntur mollitia quis libero eligendi tempore dignissimos optio recusandae.</p>
-            <p className="text-grey-700 mb-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente possimus eveniet, officia pariatur ea quibusdam iste consequuntur architecto dicta alias eius nulla eaque neque perspiciatis impedit. Nihil quidem sequi quis!</p>
+            <h2 className="text-3x1 font-bold text-gray-800 mb-3">Welcome to Globalsphere Connect</h2>
+            <p className="text-gray-700 mb-2">Get connected with the world. Share and explore news, weather updates, and connect with people worldwide.</p>
+            <p className="text-grey-700 mb-2">Experience a global community like never before. With Globalsphere Connect, you have the power to share your thoughts, experiences, and stories with a diverse audience from every corner of the globe.</p>
             <p className="text-grey-700 mb-5">
-                Why wait? Join us now and start sharing your items with your neighbor!
+                Don't miss out on the opportunity to be a part of this vibrant global network. Join us now and start connecting!
             </p>
         </section>
     )
